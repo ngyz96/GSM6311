f818e5aa0cf2f13d4b1d92c42c5c0b011345d9c1
Ng Yong Zhi <41257178+ngyz96@users.noreply.github.com>
2024-02-13 14:15:16 +0800

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -2,5 +2,6 @@
 
 for FILE in `ls -1 *.fastq.gz | sed 's/.fastq.gz//'`
 do
+    # 1134 1740 based on NCBI calculation
     seqkit seq ${FILE}.fastq.gz -M 1900 -m 1350 -j 20 -o ${FILE}_trimmed.fastq.gz
 done
