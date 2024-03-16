d0a5b040ad0d532ce235348c9a09c76f67af7dd3
Ng Yong Zhi <41257178+ngyz96@users.noreply.github.com>
2024-03-16 13:20:45 +0800

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -2,6 +2,6 @@
 
 for FILE in `ls -1 *.fastq.gz | sed 's/.fastq.gz//'`
 do
-    # 1134 1740 based on NCBI calculation
-    seqkit seq ${FILE}.fastq.gz -M 1900 -m 1350 -j 20 -o ${FILE}_trimmed.fastq.gz
+    # 1134 1740 based on NCBI calculation, 1300-1800 based on paper 
+    gunzip -c ${FILE}.fastq.gz | chopper -q 10 --minlength 1300 --maxlength 1800 --threads 12 | gzip > ${FILE}_filtered.fastq.gz;
 done
